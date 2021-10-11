2e04277e069164ef84d603b8a2210e0d63c0df5c
MonkD3 <nathoulegrand0@gmail.com>
2021-10-11 17:46:12 +0200

    Resolving rtd compilation 4

--- source/conf.py
+++ source/conf.py
@@ -33,7 +33,7 @@ html_show_sourcelink = False
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.intersphinx']
+# extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
